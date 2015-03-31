7a917f7e86c0da5e83ad5aeecdfa39ef77098a55
amandapickering <pickering.amanda@gmail.com>
2015-03-30 22:37:33 -0700

    placeholder

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
-
   $('img').on('click', function () {
     $(this).css({
       top: function () {
-        return Math.floor(Math.random() * ($(window).height() - 100));
+        return Math.floor(Math.random() * ($(window).height() - 200));
       },
       left: function () {
-        return Math.floor(Math.random() * ($(window).width() - 100));
+        return Math.floor(Math.random() * ($(window).width() - 200));
       }
     });
   });
-
 });
